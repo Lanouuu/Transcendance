bee10625f583d9195d67abe90a615d5834d03cd0
Alan De franceschi <ade-fran@f6r8s12.clusters.42paris.fr>
2025-09-16 18:28:19 +0200

    init server fastify

--- srcs/requirements/fastify/conf/index.js
+++ srcs/requirements/fastify/conf/index.js
@@ -1,1 +1,15 @@
-console.log('Hello World!');
+const fastify = require('fastify')({logger: true})
+
+const PORT = parseInt(process.env.FASTIFY_PORT, 10)
+const HOST = process.env.FASTIFY_HOST
+
+fastify.get('/', function handler (request, reply) {
+    reply.send({hello: 'Hello World!'})
+})
+
+fastify.listen({host: HOST, port: PORT}, (err) => {
+    if (err) {
+        fastify.log.error(err)
+        process.exit(1)
+    }
+})
