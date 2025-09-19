25336cc4f447b4332ac2e78dfc3986045765ffa3
Alan De franceschi <ade-fran@f6r9s10.clusters.42paris.fr>
2025-09-19 18:03:49 +0200

    up

--- srcs/requirements/fastify/conf/index.js
+++ srcs/requirements/fastify/conf/index.js
@@ -7,7 +7,7 @@ const PORT = parseInt(process.env.FASTIFY_PORT, 10)
 const HOST = process.env.FASTIFY_HOST
 
 fastify.get('/', function handler (request, reply) {
-    reply.send({hello: 'Hello World!'})
+    reply.send('Hello World!')
 })
 
 fastify.listen({host: HOST, port: PORT}, (err) => {
