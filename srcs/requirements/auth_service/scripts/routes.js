431c680e941e674b1572d4649da443b85caef045
Anthony Ciezadlik <aciezadl@f1br2s7.clusters.42paris.fr>
2025-10-08 14:43:29 +0200

    up

--- srcs/requirements/auth_service/scripts/routes.js
+++ srcs/requirements/auth_service/scripts/routes.js
@@ -1,2 +1,49 @@
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
+import jwt from "@fastify/jwt";
 
+// // Auth middleware
+// fastify.decorate("authenticate", async function(request, reply) {
+//   try {
+//     await request.jwtVerify();
+//   } catch (err) {
+//     reply.status(401).send({error: "Not authorized"});
+//   }
+// });
+
+export default async function routes(fastify, options) {
+    const db = await options.db;
+
+    fastify.get('/', function handler (request, reply) {
+        reply.send('Hello World!')
+    });
+
+    fastify.post("/signup", async (request, reply) => {
+      const { name, email, password } = request.body;
+      if(!email || !password)
+            return reply.code(400).send({error: "Mail and password required"});
+    
+      const hashedPassword = await bcrypt.hash(password, 10);
+      try {
+        const result = await db.run(
+          "INSERT INTO sessions (name, email, password) VALUES (?, ?, ?)", [name, email, hashedPassword]
+        );
+        return reply.status(201).send({id: result.lastID, name, email});
+      } catch (err) {
+        fastify.log.error(err, "Error signup");
+        return reply.status(400).send({error: "Signup failed"});
+      }
+    });
+    
+    // connexion
+    fastify.post("/login", async (request, reply) => {
+      const { email, password } = request.body;
+      const user = await db.get("SELECT * FROM sessions WHERE email = ?", [email]);
+      if (!user) return reply.status(401).send({ error: "User doesn't exist" });
+    
+      const isValid = await bcrypt.compare(password, user.password);
+      if (!isValid) return reply.status(401).send({ error: "Invalid password"});
+    
+      const token = fastify.jwt.sign({id: user.id, email: user.email });
+      return {token};
+    });
+}
--- srcs/requirements/auth_service/scripts/server.js
+++ srcs/requirements/auth_service/scripts/server.js
@@ -1,22 +1,23 @@
 import Fastify from "fastify";
 import jwt from "@fastify/jwt";
-import { initDB } from "../data/database.js";
-// import authRoutes from "../scripts/routes.js";
+import { initDB } from "./database.js";
+import authRoutes from "./routes.js";
 
 const fastify = Fastify({ logger: true });
 
-// JWT secret
+// plugin JWT
 fastify.register(jwt, { secret: "supersecretkey" });
 
 // Init DB
-const db = await initDB();
+const dbSessions = await initDB();
 
-// // Routes
-// fastify.register(authRoutes, { db });
-
-fastify.get('/', function handler (request, reply) {
-    reply.send('Hello World!')
-})
+// Routes
+fastify.register(authRoutes, { dbSessions });
 
 // Start server
-fastify.listen({ port: 8080, host: "0.0.0.0" });
+fastify.listen({ port: 8080, host: "0.0.0.0" }, (err) => {
+    if(err) {
+        fastify.log.error(err);
+        process.exit(1);
+    }
+});
