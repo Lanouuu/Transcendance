3ca391b56e8adf25f5886744b21d5efd66280ff7
Tom Lebon <tlebon@f4r11s3.clusters.42paris.fr>
2025-10-29 22:49:40 +0100

    Debut de la page account

--- srcs/requirements/gateway/www/scripts/account.ts
+++ srcs/requirements/gateway/www/scripts/account.ts
@@ -1,1 +1,20 @@
 export {};
+// export async function displayAccountPage () {
+// 	const usernameSpan	:HTMLElement | null | undefined = document.getElementById("accountBoxUsername")?.querySelector("span");
+// 	const mailSpan		:HTMLElement | null | undefined = document.getElementById("accountBoxMail")?.querySelector("span");
+// 	const dblFaSpan		:HTMLElement | null | undefined = document.getElementById("accountBox2fa")?.querySelector("span");
+
+// 	if (usernameSpan === null || usernameSpan === undefined || mailSpan === null || mailSpan === undefined || 
+// 		dblFaSpan === null || dblFaSpan === undefined){
+		
+// 	}
+
+
+// 	const res = await fetch();
+
+// 	usernameSpan.innerText = "";
+// 	mailSpan?.innerText = "";
+// 	dblFaSpan?.innerText = "";
+// }
+
+// displayAccountPage();
