20a52db9792a589e57f80372383cbc300d0fbdcd
Tom Lebon <tlebon@f6r9s8.clusters.42paris.fr>
2025-10-30 14:54:22 +0100

    Account base finie, manque juste le script

--- srcs/requirements/gateway/www/scripts/account.ts
+++ srcs/requirements/gateway/www/scripts/account.ts
@@ -1,8 +1,9 @@
-export {};
+// export {};
 // export async function displayAccountPage () {
 // 	const usernameSpan	:HTMLElement | null | undefined = document.getElementById("accountBoxUsername")?.querySelector("span");
 // 	const mailSpan		:HTMLElement | null | undefined = document.getElementById("accountBoxMail")?.querySelector("span");
 // 	const dblFaSpan		:HTMLElement | null | undefined = document.getElementById("accountBox2fa")?.querySelector("span");
+// 	const profilePic	:HTMLElement | null | undefined = document.getElementById("accountBoxProfilePic")?.querySelector("img");
 
 // 	if (usernameSpan === null || usernameSpan === undefined || mailSpan === null || mailSpan === undefined || 
 // 		dblFaSpan === null || dblFaSpan === undefined){
@@ -15,6 +16,7 @@ export {};
 // 	usernameSpan.innerText = "";
 // 	mailSpan?.innerText = "";
 // 	dblFaSpan?.innerText = "";
+// 	profilePic?.src = "";
 // }
 
 // displayAccountPage();
