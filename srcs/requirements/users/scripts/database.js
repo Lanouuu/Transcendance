224d9c5c8c686e94d76065f49df787d054ae9ced
Alan De franceschi <ade-fran@f6r13s14.clusters.42paris.fr>
2025-11-02 19:05:32 +0100

    up

--- srcs/requirements/users/scripts/database.js
+++ srcs/requirements/users/scripts/database.js
@@ -11,14 +11,14 @@ export function initDB() {
             enable2FA   BOOLEAN DEFAULT 0,
             secret2FA   TEXT,
             avatar_path TEXT NOT NULL DEFAULT 'default.png',
-            wins        INTERGER DEFAULT 0,
-            losses      INTERGER DEFAULT 0,
+            wins        INTEGER DEFAULT 0,
+            losses      INTEGER DEFAULT 0,
             created_at  DATETIME DEFAULT CURRENT_TIMESTAMP
         );
         CREATE TABLE IF NOT EXISTS friends (
-            id          INTERGER PRIMARY KEY AUTOINCREMENT,
-            user_id     INTERGER NOT NULL,
-            friends_id  INTERGER NOT NULL,
+            id          INTEGER PRIMARY KEY AUTOINCREMENT,
+            user_id     INTEGER NOT NULL,
+            friend_id   INTEGER NOT NULL,
             status      TEXT NOT NULL DEFAULT 'pending',
             created_at  DATETIME DEFAULT CURRENT_TIMESTAMP,
             UNIQUE(user_id, friend_id)
--- srcs/requirements/users/scripts/server.js
+++ srcs/requirements/users/scripts/server.js
@@ -1,8 +1,5 @@
 import Fastify from "fastify";
 import * as db from "./database.js";
-import bcrypt from "bcryptjs";
-import { authenticator } from "otplib";
-import QRCode from "qrcode";
 import cors from "@fastify/cors";
 import multipart from "@fastify/multipart";
 import fs from "fs";
@@ -10,6 +7,11 @@ import path from "path";
 
 export function runServer() {
     
+    /****************************************************************************/
+    /*                       Init Fastify Users Server                          */
+    /****************************************************************************/
+
+    //#region init_users_server
     const fastify = Fastify({ logger: true });
     const PORT = parseInt(process.env.USERS_PORT, 10);
     const HOST = process.env.USERS_HOST;
@@ -24,7 +26,15 @@ export function runServer() {
     fastify.register(multipart, {
       limits: { fileSize: 2 * 1024 * 1024 },
     });
+
+    //#endregion init_users_server
     
+    /****************************************************************************/
+    /*                             Create Users                                 */
+    /****************************************************************************/
+
+    //#region create_user
+
     fastify.post("/create_user", async (request, reply) => {
       const { name, mail, password, enable2FA, secret2FA } = request.body;
 
@@ -41,7 +51,6 @@ export function runServer() {
       }
     });
 
-    // Route pour récupérer un user par mail (utilisée par auth_service)
     fastify.get("/mail/:mail", (req, reply) => {
       try {
         const stmt = usersDB.prepare("SELECT * FROM users WHERE mail = ?");
@@ -59,34 +68,49 @@ export function runServer() {
       }
     });
 
+    //#endregion create_user
+
+    /****************************************************************************/
+    /*                       Users Avatars Management                           */
+    /****************************************************************************/
+
+    //#region avatar_management
+
     fastify.post("/upload-avatar", async (req, reply) => {
-      const data = await req.file();
-      if (!data) {
-        return reply.status(400).send({ error: "No file uploaded" });
+      try {
+
+        const data = await req.file();
+        if (!data) {
+          return reply.status(400).send({ error: "No file uploaded" });
+        }
+  
+        const { id } = data.fields;
+        if (!id) {
+          return reply.status(400).send({ error: "ID required" });
+        }
+  
+        const allowedTypes = ["image/jpeg", "image/png"];
+        if (!allowedTypes.includes(data.mimetype)) {
+          return reply.status(400).send({ error: "Invalid file type" });
+        }
+  
+        const extension = data.mimetype === "image/png" ? ".png" : ".jpg";
+        const fileName = `${id}${extension}`;
+        const filePath = `/data/avatars/${fileName}`;
+        const fullPath = path.join(process.cwd(), filePath);
+  
+        const stream = fs.createWriteStream(fullPath);
+        await data.file.pipe(stream);
+  
+        const stmt = usersDB.prepare("UPDATE users SET avatar_path = ? WHERE id = ?");
+        stmt.run(fileName, id);
+  
+        reply.send({ success: true, avatar:fileName});
+
+      } catch (err) {
+        fastify.log.error(err);
+        return reply.status(500).send({ error: "Internal Server Error" });
       }
-
-      const { id } = data.fields;
-      if (!id) {
-        return reply.status(400).send({ error: "ID required" });
-      }
-
-      const allowedTypes = ["image/jpeg", "image/png"];
-      if (!allowedTypes.includes(data.mimetype)) {
-        return reply.status(400).send({ error: "Invalid file type" });
-      }
-
-      const extension = data.mimetype === "image/png" ? ".png" : ".jpg";
-      const fileName = `${id}${extension}`;
-      const filePath = `/data/avatars/${fileName}`;
-      const fullPath = path.join(process.cwd(), filePath);
-
-      const stream = fs.createWriteStream(fullPath);
-      await data.file.pipe(stream);
-
-      const stmt = usersDB.prepare("UPDATE users SET avatar_path = ? WHERE id = ?");
-      stmt.run(fileName, id);
-
-      reply.send({ success: true, avatar:fileName})
 
     });
 
@@ -116,6 +140,14 @@ export function runServer() {
         return reply.status(500).send({ error: "Internal Server Error" });
       }
     });
+
+    //#endregion avatar_management
+ 
+    /****************************************************************************/
+    /*                           Get Users Data                                 */
+    /****************************************************************************/
+
+    //#region get_users_data
 
     fastify.get("/get-user/:id", async (req, reply) => {
       const { id } = req.params;
@@ -148,11 +180,59 @@ export function runServer() {
       }
 
     });
+    
+    //#endregion get_users_data
+
+    /****************************************************************************/
+    /*                       Users Friends Management                           */
+    /****************************************************************************/
+
+    //#region friends_management
+
+    fastify.post("/send-invit/:id/:friendName", async (req, reply) => {
+      try {
+        
+        const userID = req.params.id;
+        const friendName = req.params.friendName;
+        if (!userID) {
+          return reply.status(400).send({ error: "ID required" });
+        } else if (!friendName) {
+          return reply.status(400).send({ error: "friend name required" });
+        }
+  
+        const getStmt = usersDB.prepare("SELECT id FROM users WHERE name = ?");
+        const row = getStmt.get(friendName);
+        if (!row) {
+          return reply.status(404).send({ error: "User (friend) not found" });
+        }
+        const friendID = row.id;
+        if (Number(userID) === friendID) {
+          return reply.status(400).send({ error: "You cannot invite yourself" });
+        }
+  
+        const checkStmt = usersDB.prepare(`SELECT status FROM friends WHERE user_id = ? AND friend_id = ?`);
+        const existing = checkStmt.get(userID, friendID);
+        if (existing) {
+          return reply.status(409).send({ message: "Invitation already sent" });
+        }
+  
+        const insertStmt = usersDB.prepare("INSERT INTO friends (user_id, friend_id, status, created_at) VALUES (?, ?, 'pending', datetime('now'))");
+        insertStmt.run(userID, friendID);
+
+        return reply.status(201).send({ success: true });
+
+      } catch (err) {
+        fastify.log.error(err);
+        return reply.status(500).send({ error: "Internal Server Error" });
+      }
+    });
+
+    //#endregion friends_management
 
     fastify.listen({host: HOST, port: PORT}, (err) => {
-        if (err) {
-            fastify.log.error(err);
-            process.exit(1);
-        }
+      if (err) {
+          fastify.log.error(err);
+          process.exit(1);
+      }
     });
 }
