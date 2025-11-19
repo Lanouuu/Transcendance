eb527a44a6852291c85f88af7bc0af28293ea07f
Tom Lebon <tlebon@f4r10s10.clusters.42paris.fr>
2025-11-19 11:48:59 +0100

    Dernier fix pour le hot reload --> Ajout de index.html aux watch de compilation

--- srcs/requirements/gateway/conf/watch-content.sh
+++ srcs/requirements/gateway/conf/watch-content.sh
@@ -5,6 +5,7 @@ cd /etc/nginx/html
 inotifywait -m -q -r -e modify,create,delete \
   ./pages \
   ./scripts \
+  ./index.html  \
   --exclude '\.git|node_modules|\.swp' 2>/dev/null | while read path action file; do
   echo "🔄 $file changed at $(date +%T), recompiling..."
   tailwindcss -i ./css/style.css -o ./css/output.css --minify
